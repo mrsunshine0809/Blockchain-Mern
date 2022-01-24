6c579042a193adf6b7afe175c17f4e4933364478
mrsunshine0809 <devsunshine89@gmail.com>
2022-01-24 23:18:26 +0200

    First initial commit

--- clientF/components/Weather/index.js
+++ clientF/components/Weather/index.js
@@ -67,7 +67,7 @@ function Weather() {
       {!type ? (
         <CircularProgress />
       ) : (
-        <div>
+        <>
           <Typography className={styles.title} variant="body2" gutterBottom>
             wind: {windSpeed} {windDeg}
           </Typography>
@@ -79,21 +79,20 @@ function Weather() {
             description: {descr}, {main}
             <Avatar src={imgMain} />
           </Typography>
-          <CardContent>
-            <Typography variant="body2" gutterBottom>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              luctus ut est sed faucibus. Duis bibendum ac ex vehicula laoreet.
-              Suspendisse congue vulputate lobortis. Pellentesque at interdum
-              tortor. Quisque arcu quam, malesuada vel mauris et, posuere
-              sagittis ipsum. Aliquam ultricies a ligula nec faucibus. In elit
-              metus, efficitur lobortis nisi quis, molestie porttitor metus.
-              Pellentesque et neque risus. Aliquam vulputate, mauris vitae
-              tincidunt interdum, mauris mi vehicula urna, nec feugiat quam
-              lectus vitae ex.
-            </Typography>
-          </CardContent>
-        </div>
+        </>
       )}
+      <CardContent>
+        <Typography variant="body2" gutterBottom>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus
+          ut est sed faucibus. Duis bibendum ac ex vehicula laoreet. Suspendisse
+          congue vulputate lobortis. Pellentesque at interdum tortor. Quisque
+          arcu quam, malesuada vel mauris et, posuere sagittis ipsum. Aliquam
+          ultricies a ligula nec faucibus. In elit metus, efficitur lobortis
+          nisi quis, molestie porttitor metus. Pellentesque et neque risus.
+          Aliquam vulputate, mauris vitae tincidunt interdum, mauris mi vehicula
+          urna, nec feugiat quam lectus vitae ex.
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
